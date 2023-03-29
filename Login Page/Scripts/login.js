4525e16028618282aa4e5ce074c67a399ccf63f5
yazeedbukhari <96405201+yazeedbukhari@users.noreply.github.com>
2023-03-29 14:46:40 +0000

    carried code over from local, will be using github a much as possiible now

---
+++ Login Page/Scripts/login.js
@@ -0,0 +1,21 @@
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // Authenticate the user with the email and password inputs
+  login(email, password);
+});
+
+function login(email, password) {
+  // Check if the email and password are valid
+  if (email === "example@example.com" && password === "password") {
+    // If valid, redirect to the dashboard page
+    window.location.href = "dashboard.html";
+  } else {
+    // If invalid, display an error message
+    alert("Invalid email or password.");
+  }
+}
